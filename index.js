4c6ba43d49dd425de98b7f924475784dc368b9ca
Edward Marion <edmarion6@gmail.com>
2022-09-10 00:28:06 -0400

    commit tree 7b5f1062a640ef24ce5c6798d705009e34fa9988

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity) {
+        return 'This Saturday, I want to bathe my dog!';
+    } else {
+        return 'This Saturday, I want to roller-skate!';
+    }
+}
+
+const home = 'work from home.';
+const work = 'go to the office.';
+function mondayWork(home) {
+       return `This Monday, I will ${work}`
+}    
